f56f50ca3304f3fbeeafebf33e19a688a0382ea2
David <dgonzalezdiez@gmail.com>
2014-08-31 11:11:14 +0200

    1.0

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Presentación de David González">
+    <meta name="author" content="David González">
+    <title>Penguinjournas, David González</title>
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+  <div id="content">
+    <h1>Plan, track and adapt</h1>
+    <p>Soy David González, trabajo entre el desarrollo de software y las operaciones. Obsesionado con que las cosas funcionen.</p>
+    <p>Vivo en Donostia, estudié Ingeniería Técnica en informática de sistemas, he trabajado <a href="http://arista.es" alt="Arista interactiva">Arista interactiva</a>, <a href="http://diariovasco.com" alt="Diariovasco.com">diariovasco.com</a>, <a href="http://dvbus.info" alt="DVbus canal de información urbana">dvbus.info</a>, <a href="http://hispavista.com" alt="Hispavista">Hispavista</a>, <a href="http://binovo.es" alt="Binovo">Binovo</a> y actualmente compagino mi trabajo en <a href="http://nemsolutions.com" alt="NEM Solutions">NEM Solutions</a> con colaboraciones con <a href="http://buzzko.com">Buzzko Marketing</a>.</p>
+    <p>En mi día a día trabajo buscando soluciones dirigidas al manejo de grandes volúmenes de información y las mejoras contínuas en aplicaciones web relacionadas con su rendimiento. Por otro lado parte de mi trabajo está enfocado a la mejora del ciclo de desarrollo de software en nuestro equipo adoptando buenas prácticas como la integración contínua, métricas, et cetera. </p>
+    <div id="enlaces">
+    <a href="http://twitter.com/penguinjournals"><img src="images/icons/Twitter.png"></a>
+    <a href="https://www.linkedin.com/in/dgonzalezdiez"><img src="images/icons/LinkedIn.png"></a>
+    <a href="https://github.com/penguinjournals"><img src="images/icons/GitHub.png"></a>
+    <a href="http://programania.net"><img src="images/icons/WordPress.png"></a>
+    <img src="images/icons/Gmail.png">dgonzalezdiez@gmail.com
+    </div>
+  </div>
+  </body>
+</html>
