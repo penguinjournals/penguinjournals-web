13039a7a371dedac15e6d123f3130dd1427f6f83
David <dgonzalezdiez@gmail.com>
2014-08-31 11:44:12 +0200

    1.2

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Presentación de David González">
+    <meta name="keywords" content="David González, curriculum, desarrollo de software, operaciones, integracion contínua">
+    <meta name="description" content="David González, curriculum, desarrollo de software, operaciones, integración contínua">
     <meta name="author" content="David González">
     <title>Penguinjournas, David González</title>
     <link href="css/style.css" rel="stylesheet">
@@ -16,13 +17,24 @@
     <p>Soy David González, trabajo entre el desarrollo de software y las operaciones. Me gusta que las cosas funcionen bien.</p>
     <p>Vivo en Donostia, estudié Ingeniería Técnica en informática de sistemas, he trabajado <a href="http://arista.es" alt="Arista interactiva">Arista interactiva</a>, <a href="http://diariovasco.com" alt="Diariovasco.com">diariovasco.com</a>, <a href="http://dvbus.info" alt="DVbus canal de información urbana">dvbus.info</a>, <a href="http://hispavista.com" alt="Hispavista">Hispavista</a>, <a href="http://binovo.es" alt="Binovo">Binovo</a> y actualmente compagino mi trabajo en <a href="http://nemsolutions.com" alt="NEM Solutions">NEM Solutions</a> con colaboraciones con <a href="http://buzzko.com">Buzzko Marketing</a>.</p>
     <p>En mi día a día trabajo buscando soluciones dirigidas al manejo de grandes volúmenes de información y las mejoras contínuas en aplicaciones web relacionadas con su rendimiento. Por otro lado parte de mi trabajo está enfocado a la mejora del ciclo de desarrollo de software en nuestro equipo adoptando buenas prácticas como la integración contínua, métricas, et cetera. </p>
-    <div id="enlaces">
+    <p>En mis ratos libres discuto, y mucho, con <a href="http://twitter.com/artolamola">@artolamola</a> y <a href="http://twitter.com/ggalmazor">@ggalmazor</a> en <a href="http://programania.net">programania</a>, nuestro "cuaderno de software" particular.</p>
+    <p>
     <a href="http://twitter.com/penguinjournals"><img src="images/icons/Twitter.png"></a>
     <a href="https://www.linkedin.com/in/dgonzalezdiez"><img src="images/icons/LinkedIn.png"></a>
     <a href="https://github.com/penguinjournals"><img src="images/icons/GitHub.png"></a>
     <a href="http://programania.net"><img src="images/icons/WordPress.png"></a>
     <img src="images/icons/Gmail.png">dgonzalezdiez@gmail.com
-    </div>
+    </p>
   </div>
   </body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-5271637-1', 'auto');
+  ga('send', 'pageview');
+
+</script>  
 </html>
