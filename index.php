5f8c4c6a194280a7023a711495b2884e5d6ab539
David González <dgonzalezdiez@ticketbis.com>
2015-08-11 10:50:18 +0200

    Updated to include my new job at Ticketbis

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="David González, curriculum, desarrollo de software, operaciones, integracion contínua">
     <meta name="description" content="David González, curriculum, desarrollo de software, operaciones, integración contínua">
     <meta name="author" content="David González">
-    <title>Penguinjournas, David González</title>
+    <title>Penguinjournals, David González</title>
     <link href="css/style.css" rel="stylesheet">
   </head>
 
@@ -15,8 +15,8 @@
   <div id="content">
     <h1>Plan, track and adapt</h1>
     <p>Soy David González, trabajo entre el desarrollo de software y las operaciones. Me gusta que las cosas funcionen bien.</p>
-    <p>Vivo en Donostia, estudié Ingeniería Técnica en informática de sistemas, he trabajado <a href="http://arista.es" alt="Arista interactiva">Arista interactiva</a>, <a href="http://diariovasco.com" alt="Diariovasco.com">diariovasco.com</a>, <a href="http://dvbus.info" alt="DVbus canal de información urbana">dvbus.info</a>, <a href="http://hispavista.com" alt="Hispavista">Hispavista</a>, <a href="http://binovo.es" alt="Binovo">Binovo</a> y actualmente compagino mi trabajo en <a href="http://nemsolutions.com" alt="NEM Solutions">NEM Solutions</a> con colaboraciones con <a href="http://buzzko.com">Buzzko Marketing</a>.</p>
-    <p>En mi día a día trabajo buscando soluciones dirigidas al manejo de grandes volúmenes de información y las mejoras contínuas en aplicaciones web relacionadas con su rendimiento. Por otro lado parte de mi trabajo está enfocado a la mejora del ciclo de desarrollo de software en nuestro equipo adoptando buenas prácticas como la integración contínua, métricas, et cetera. </p>
+    <p>Vivo en Donostia, estudié Ingeniería Técnica en informática de sistemas, he trabajado <a href="http://arista.es" alt="Arista interactiva">Arista interactiva</a>, <a href="http://diariovasco.com" alt="Diariovasco.com">diariovasco.com</a>, <a href="http://dvbus.info" alt="DVbus canal de información urbana">dvbus.info</a>, <a href="http://hispavista.com" alt="Hispavista">Hispavista</a>, <a href="http://binovo.es" alt="Binovo">Binovo</a>,<a href="http://nemsolutions.com" alt="NEM Solutions">NEM Solutions</a> y actualmente trabajo en <a href="http://ticketbis.com" alt="Ticketbis">Ticketbis</a> y cuando puedo ayudo a <a href="http://twitter.com/lacajadezapatos">@lacajadezapatos</a> y a <a href="http://twitter.com/mallimi">@mallimi</a> en su aventura <a href="http://buzzko.com">Buzzko Marketing</a>.</p>
+    <p>Creo que todo el mundo está implicado en el ciclo de desarrollo de un producto de software, desde negocio hasta sistemas pasando por diseño o desarrollo. No me gusta escuchar expresiones como "ese es un problema de sistemas" o "la culpa la tienen los de desarrollo". Por eso intento trabajar profundamente ciclos de desarrollo que hagan partícipe a todo el equipo. Para ello quiero que el equipo se apoye en buenas prácticas como la integración contínua, el uso de métricas, la mejora de sus capacidades técnicas et cetera.</p> 
     <p>En mis ratos libres discuto, y mucho, con <a href="http://twitter.com/artolamola">@artolamola</a> y <a href="http://twitter.com/ggalmazor">@ggalmazor</a> en <a href="http://programania.net">programania</a>, nuestro "cuaderno de software" particular.</p>
     <p>
     <a href="http://twitter.com/penguinjournals"><img src="images/icons/Twitter.png"></a>
